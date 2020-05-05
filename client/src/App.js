75011eb13d65396f60fa6f60c381c3754f7e9d81
sakchipl <sakar.chiplunkar@publicissapient.com>
2020-05-05 13:11:24 +0530

    Add register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 const App = () => (
+  <BrowserRouter>
     <Fragment>
       <Navbar />
-      <Landing />
+      <Route exact path="/" component={Landing}/>
+      <section className="container">
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </Fragment>
+  </BrowserRouter>
 )
 
 export default App;
